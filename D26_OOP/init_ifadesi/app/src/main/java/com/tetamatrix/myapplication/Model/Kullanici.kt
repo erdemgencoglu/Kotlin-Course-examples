c296f4101b035670e46d76dd3c58b926936add74
Erdem Gençoğlu <gencogluerdem@gmail.com>
2021-06-02 13:15:44 +0300

    D26-1

---
+++ D26_OOP/init_ifadesi/app/src/main/java/com/tetamatrix/myapplication/Model/Kullanici.kt
@@ -0,0 +1,17 @@
+package com.tetamatrix.myapplication.Model
+
+class Kullanici {
+    var isim: String? = null
+    var yas: Int? = null
+    //initten sonra çağırılır
+    constructor(isim:String,yas:Int){
+        this.isim=isim
+        this.yas=yas
+        println("constructer çağırıldı.")
+    }
+    //ilk çağırılır
+    init {
+        println("init çağırıldı.")
+    }
+
+}
