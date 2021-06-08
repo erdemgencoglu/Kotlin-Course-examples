ed575168150ddd84811b1725489ce0f3d36f344a
Erdem Gençoğlu <gencogluerdem@gmail.com>
2021-06-08 11:12:57 +0300

    cd

--- PhotoShared/app/src/main/java/com/tetamatrix/photoshared/Ui/PostActivity.kt
+++ PhotoShared/app/src/main/java/com/tetamatrix/photoshared/Ui/PostActivity.kt
@@ -1,15 +1,47 @@
 package com.tetamatrix.photoshared.Ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.tetamatrix.photoshared.LoginActivity
+import com.tetamatrix.photoshared.R
 import com.tetamatrix.photoshared.databinding.ActivityPostBinding
 
 class PostActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityPostBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPostBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    //menuyu oluşturma
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.shareItem) {
+            //Post Share Activity
+            val intent = Intent(this, PostShareActivity::class.java)
+            startActivity(intent)
+        } else if (item.itemId == R.id.exitItem) {
+            //Çıkış ve Login Activity
+            auth.signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ PhotoShared/app/src/main/java/com/tetamatrix/photoshared/Ui/PostShareActivity.kt
@@ -0,0 +1,17 @@
+package com.tetamatrix.photoshared.Ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.tetamatrix.photoshared.R
+import com.tetamatrix.photoshared.databinding.ActivityPostBinding
+
+class PostShareActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPostBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPostBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
